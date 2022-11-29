1c047bfc76c57e446b04f29c1ee4b125cbe1d8ed
Joris Dral <joris@well-typed.com>
2022-11-29 08:53:29 +0100

    Enable writing custom `InterpretOp` instances.

--- src/Test/QuickCheck/StateModel/Lockstep/Op.hs
+++ src/Test/QuickCheck/StateModel/Lockstep/Op.hs
@@ -3,6 +3,7 @@ module Test.QuickCheck.StateModel.Lockstep.Op (
   , InterpretOp(..)
   , WrapRealized(..)
   , intOpRealizedId
+  , intOpTransformer
   ) where
 
 import Test.QuickCheck.StateModel (Realized)
@@ -27,8 +28,8 @@ class Operation op => InterpretOp op f where
 {-------------------------------------------------------------------------------
   Interop with 'Realized'
 
-  We want to execute operations against 'Realized' values, but since that is
-  a newtype, we need to wrap and unwrap in order to be able to give the
+  We want to execute operations against 'Realized' values, but since that is a
+  type family, we need to wrap and unwrap in order to be able to give the
   appropriate 'InterpretOp' instances.
 -------------------------------------------------------------------------------}
 
@@ -44,3 +45,22 @@ intOpRealizedId ::
   => (op a b -> a -> Maybe b)
   -> op a b -> WrapRealized m a -> Maybe (WrapRealized m b)
 intOpRealizedId = coerce
+
+-- | Convenience function for defining 'InterpretOp' instances for monad
+-- transformer stacks.
+intOpTransformer ::
+     forall t m a b op.
+     ( Realized (t m) a ~ Realized m a
+     , Realized (t m) b ~ Realized m b
+     , InterpretOp op (WrapRealized m)
+     )
+  => op a b
+  -> WrapRealized (t m) a
+  -> Maybe (WrapRealized (t m) b)
+intOpTransformer op wr = coerceOut <$> intOp op (coerceIn wr)
+  where
+    coerceIn :: WrapRealized (t m) a -> WrapRealized m a
+    coerceIn = coerce
+
+    coerceOut :: WrapRealized m b -> WrapRealized (t m) b
+    coerceOut = coerce
