0b014533585de3f7152226d9d8024370ba2f33ab
Joris Dral <joris@well-typed.com>
2025-01-15 10:42:57 +0100

    Golden tests for counterexamples produced by `postcondition` and `postconditionWith`

--- test/Main.hs
+++ test/Main.hs
@@ -1,14 +1,16 @@
 module Main (main) where
 
-import Test.Tasty
+import           Test.Tasty
 
-import Test.IORef.Full   qualified
-import Test.IORef.Simple qualified
-import Test.MockFS       qualified
+import           Test.Golden
+import           Test.IORef.Full
+import           Test.IORef.Simple
+import           Test.MockFS
 
 main :: IO ()
 main = defaultMain $ testGroup "quickcheck-lockstep" [
-      Test.IORef.Simple.tests
+      Test.Golden.tests
+    , Test.IORef.Simple.tests
     , Test.IORef.Full.tests
     , Test.MockFS.tests
     ]
